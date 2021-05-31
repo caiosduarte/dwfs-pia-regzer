d7b66b8c487bef6a7ba539819ec84bd45fc9a604
Caio Duarte's UX305U <caiosduarte@gmail.com>
2021-05-31 14:40:41 -0300

    Reset de senha consertado: apagando o token antigo e atualizando a senha do usuário via ORM

--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -1,9 +1,5 @@
 import { hash } from "bcrypt";
-import { getRepository } from "typeorm";
 import AppError from "../../../errors/AppError";
-import Token from "../models/Token";
-import User from "../models/User";
-import UsersRepository from "../repositories/implementations/UsersRepository";
 import ITokensRepository from "../repositories/ITokensRepository";
 
 interface IRequest {
@@ -15,37 +11,9 @@ export default class ResetPasswordService {
     constructor(private repository: ITokensRepository) {}
 
     async execute({ tokenEncoded, password }: IRequest): Promise<void> {
-        const usersRepository = UsersRepository.getInstance();
-        const userFromToken = await usersRepository.findByToken(
-            "fe84818c-dd94-473a-8dcf-ef7e6f40cacc"
-        );
-
-        console.log("User => ", userFromToken);
-
-        if (userFromToken) {
-            const tokens = userFromToken.tokens;
-            const token = tokens?.shift();
-
-            console.log("Token => ", token);
-
-            //delete userFromToken.tokens;
-            usersRepository.save(userFromToken);
-            console.log("User => ", userFromToken);
-        }
-
-        //const userFromArray = new User();
-
-        //Object.assign(userFromArray, arrayUsers[0] as User);
-
-        //console.log("userByToken => ", userFromArray);
-
-        /*
-
         const token = await this.repository.findByEncoded(tokenEncoded);
 
         const user = token?.user;
-
-        console.log("Token => $", token);
 
         if (!token || !user) {
             throw new AppError("Token invalid!");
@@ -67,10 +35,10 @@ export default class ResetPasswordService {
 
         const tokenId = token.id;
 
+        // salva a senha do usuário
         await this.repository.save(token);
 
-        //await this.repository.deleteById(tokenId);
-
-        */
+        // remove o token antigo
+        await this.repository.deleteById(tokenId);
     }
 }
