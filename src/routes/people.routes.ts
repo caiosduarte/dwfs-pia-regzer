b07e61e21c1857c0a5c554b097492d53847660f3
Caio Duarte's UX305U <caiosduarte@gmail.com>
2021-06-05 12:30:55 -0300

    multer adicionado para envio de arquivos

--- src/routes/people.routes.ts
+++ src/routes/people.routes.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
+import multer from "multer";
 import ensureAuthenticated from "../middlewares/ensureAuthenticated";
 
 const peopleRoutes = Router();
 
-peopleRoutes.post("/document", ensureAuthenticated, (request, response) => {
-    const { file } = request.body;
+const upload = multer({ dest: "./tmp" });
+
+peopleRoutes.use(ensureAuthenticated);
+
+peopleRoutes.post("/document", upload.array("file"), (request, response) => {
+    const { file } = request;
     const { name } = request.query;
     const { id } = request.user;
+
+    console.log(`file ${file} - name ${name} - id ${id} `);
 
     return response.status(201).send();
 });
