3c08279f8e37c17ed1deedebacaed9d50846d632
Caio Duarte's UX305U <caiosduarte@gmail.com>
2021-06-22 12:09:32 -0300

    tsconfig melhorado

--- dist/modules/people/controllers/CreatePersonController.js
+++ dist/modules/people/controllers/CreatePersonController.js
@@ -1,1 +1,8 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CreatePersonController = /** @class */ (function () {
+    function CreatePersonController() {
+    }
+    return CreatePersonController;
+}());
+exports.default = CreatePersonController;
--- dist/modules/people/services/CreatePersonService.js
+++ dist/modules/people/services/CreatePersonService.js
@@ -1,1 +1,8 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CreatePersonService = /** @class */ (function () {
+    function CreatePersonService() {
+    }
+    return CreatePersonService;
+}());
+exports.default = CreatePersonService;
