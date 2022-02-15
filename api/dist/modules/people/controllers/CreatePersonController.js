8e041823a57ee6d715123d18a8e7a66a25d04950
Caio Duarte's UX305U <caiosduarte@gmail.com>
2022-02-15 17:21:23 -0300

    diretório dist removido

--- api/dist/modules/people/controllers/CreatePersonController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var CreatePersonController = (function () {
-    function CreatePersonController() {
-    }
-    return CreatePersonController;
-}());
-exports.default = CreatePersonController;
--- api/dist/modules/people/services/CreatePersonService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var CreatePersonService = (function () {
-    function CreatePersonService() {
-    }
-    return CreatePersonService;
-}());
-exports.default = CreatePersonService;
